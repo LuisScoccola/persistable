714128a532e5c3a566ec39e4af1d04f6e6b34721
Luis Scoccola <luis.scoccola@gmail.com>
2022-06-22 18:20:12 -0400

    clean code.

--- linkage/plot.py
+++ linkage/plot.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 
 
+# a combination of:
 # https://stackoverflow.com/a/65604940/2171328
 # https://stackoverflow.com/a/47166787/7128154
 # https://matplotlib.org/3.3.3/api/collections_api.html#matplotlib.collections.PathCollection
@@ -9,24 +10,7 @@ import matplotlib as mpl
 # https://stackoverflow.com/questions/36730261/matplotlib-path-contains-point
 # https://stackoverflow.com/a/36335048/7128154
 class StatusbarHoverManager:
-    """
-    Manage hover information for mpl.axes.Axes object based on appearing
-    artists.
 
-    Attributes
-    ----------
-    ax : mpl.axes.Axes
-        subplot to show status information
-    artists : list of mpl.artist.Artist
-        elements on the subplot, which react to mouse over
-    labels : list (list of strings) or strings
-        each element on the top level corresponds to an artist.
-        if the artist has items
-        (i.e. second return value of contains() has key 'ind'),
-        the element has to be of type list.
-        otherwise the element if of type string
-    cid : to reconnect motion_notify_event
-    """
     def __init__(self, ax, xlabel, ylabel):
         assert isinstance(ax, mpl.axes.Axes)
 
@@ -55,7 +39,8 @@ class StatusbarHoverManager:
         def hover(event):
             if event.inaxes != self.ax:
                 return
-            info = (str(self.xlabel)+'={:.3e}, ' + str(self.ylabel)+'={:.3e}').format(event.xdata, event.ydata)
+            #info = (str(self.xlabel)+'={:.3e}, ' + str(self.ylabel)+'={:.3e}').format(event.xdata, event.ydata)
+            info = (str(self.xlabel)+'={:.3e}').format(event.xdata)
             for aa, artist in enumerate(self.artists):
                 cont, dct = artist.contains(event)
                 if not cont:
