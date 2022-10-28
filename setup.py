850af1d90631f2b5897ec1ac030c3b65efdbe024
Luis Scoccola <luis.scoccola@gmail.com>
2022-10-28 12:32:39 -0400

    perf: do not use cython line trace unless we are running coverage tests

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 import warnings
+import sys
+
+# We only want linetrace for profiling cython when running coverage tests, and
+# do not compile with this option in all other cases.
+# The following may not be the ideal way to do this, but it works for now.
+# We may want to switch to something like:
+# https://docs.python.org/3/distutils/apiref.html#creating-a-new-distutils-command
+if "--compile-with-cython-linetrace" in sys.argv:
+    cython_trace = True
+    sys.argv.remove("--compile-with-cython-linetrace")
+else:
+    cython_trace = False
+
 
 try:
     from Cython.Distutils import build_ext
-    from setuptools import setup, Extension, find_packages
+    from setuptools import setup, Extension
 
     HAVE_CYTHON = True
 
-    from Cython.Compiler.Options import get_directive_defaults
-
-    directive_defaults = get_directive_defaults()
-    directive_defaults["linetrace"] = True
-    directive_defaults["binding"] = True
+    if cython_trace:
+        from Cython.Compiler.Options import get_directive_defaults
+        directive_defaults = get_directive_defaults()
+        directive_defaults["linetrace"] = True
+        directive_defaults["binding"] = True
 
 except ImportError as e:
     warnings.warn(e.args[0])
@@ -34,36 +47,40 @@ class CustomBuildExtCommand(build_ext):
         self.include_dirs.append(numpy.get_include())
         build_ext.run(self)
 
+if cython_trace:
+    define_macros = [("CYTHON_TRACE_NOGIL", "1")]
+else:
+    define_macros = []
 
 auxiliary = Extension(
     "persistable.auxiliary",
     sources=["persistable/auxiliary.pyx"],
-    define_macros=[("CYTHON_TRACE_NOGIL", "1")],
+    define_macros=define_macros,
 )
 relabel_dendrogram = Extension(
     "persistable.borrowed.relabel_dendrogram",
     sources=["persistable/borrowed/relabel_dendrogram.pyx"],
-    define_macros=[("CYTHON_TRACE_NOGIL", "1")],
+    define_macros=define_macros,
 )
 dense_mst = Extension(
     "persistable.borrowed.dense_mst",
     sources=["persistable/borrowed/dense_mst.pyx"],
-    define_macros=[("CYTHON_TRACE_NOGIL", "1")],
+    define_macros=define_macros,
 )
 dist_metrics = Extension(
     "persistable.borrowed.dist_metrics",
     sources=["persistable/borrowed/dist_metrics.pyx"],
-    define_macros=[("CYTHON_TRACE_NOGIL", "1")],
+    define_macros=define_macros,
 )
 prim_mst = Extension(
     "persistable.borrowed.prim_mst",
     sources=["persistable/borrowed/prim_mst.pyx"],
-    define_macros=[("CYTHON_TRACE_NOGIL", "1")],
+    define_macros=define_macros,
 )
 _hdbscan_boruvka = Extension(
     "persistable.borrowed._hdbscan_boruvka",
     sources=["persistable/borrowed/_hdbscan_boruvka.pyx"],
-    define_macros=[("CYTHON_TRACE_NOGIL", "1")],
+    define_macros=define_macros,
 )
 
 if not HAVE_CYTHON:
@@ -71,7 +88,7 @@ if not HAVE_CYTHON:
 
 setup(
     name="persistable",
-    version="0.3.1",
+    version="0.3.2",
     description="Persistable: persistent and stable clustering. Versatile density-based clustering for exploratory data analysis.",
     license="3-clause BSD",
     maintainer="Luis Scoccola",
