1c842d0862314b6ffffa40a92bf9558733db9120
Luis Scoccola <luis.scoccola@gmail.com>
2022-11-02 17:00:30 -0400

    try 1

--- persistable/test/test_ui.py
+++ persistable/test/test_ui.py
@@ -13,34 +13,41 @@ def setup():
     global url
     global default_params
 
-    X = make_blobs(2000, centers=4, random_state=1)[0]
+    X = make_blobs(100, centers=2, random_state=1)[0]
     p = persistable.Persistable(X)
     pi = persistable.PersistableInteractive(p)
     port = pi.start_UI()
     url = "http://localhost:" + str(port) + "/"
 
     default_params = {
-        'n_clusters': 1,
-        'start': [1.1182295907794053, 0.09969876992582069],
-        'end': [2.2421354738977923, 0.0497231839883207]
+        "n_clusters": 1,
+        "start": [2.6996237730192476, 0.33253216984296086],
+        "end": [5.412951220013212, 0.16584515812421086],
     }
 
     yield
-
 
 
 # buttons
 def ccf_compute_button_locator(page):
     return page.locator("button#compute-ccf-button-")
 
+
 def ccf_vineyard_input_selection_radio_on_locator(page):
-    return page.locator("div#ccf-plot-controls-div-.parameters div#display-lines-selection- :text('On')")
+    return page.locator(
+        "div#ccf-plot-controls-div-.parameters div#display-lines-selection- :text('On')"
+    )
+
 
 def pv_compute_button_locator(page):
     return page.locator("button#compute-pv-button-")
 
+
 def pv_parameter_selection_radio_on_locator(page):
-    return page.locator("div#pv-plot-controls-div-.parameters div#display-parameter-selection- :text('On')")
+    return page.locator(
+        "div#pv-plot-controls-div-.parameters div#display-parameter-selection- :text('On')"
+    )
+
 
 def pv_export_parameters_button_locator(page):
     return page.locator("button#export-parameters-")
@@ -50,20 +57,24 @@ def pv_export_parameters_button_locator(page):
 def ccf_controls_div_locator(page):
     return page.locator("div#ccf-plot-controls-div-")
 
+
 def ccf_density_threshold_label_locator(page):
     return page.locator("div#ccf-plot- text[data-unformatted='Density threshold']")
+
 
 def ccf_1st_line_start_label_locator(page):
     return page.locator("div#ccf-plot- text[data-unformatted='1st line start']")
 
+
 def pv_prominence_label_locator(page):
     return page.locator("div#pv-plot- text[data-unformatted='Prominence']")
+
 
 def pv_parameter_selection_locator(page):
     return page.locator("div#pv-plot-controls-div- div#parameter-selection-div-")
 
 
-def test_app_title(page : Page):
+def test_app_title(page: Page):
     global pi
     global url
 
@@ -71,14 +82,14 @@ def test_app_title(page : Page):
     expect(page).to_have_title(re.compile("Persistable"))
 
 
-#def test_end_to_end(page : Page):
-#
-#    page.goto(url)
-#    
-#    expect(ccf_density_threshold_label_locator(page)).not_to_be_visible()
-#    expect(ccf_controls_div_locator(page)).not_to_be_visible()
-#    ccf_compute_button_locator(page).click()
-#    expect(ccf_density_threshold_label_locator(page)).to_be_visible()
+def test_end_to_end(page: Page):
+
+    page.goto(url)
+
+    expect(ccf_density_threshold_label_locator(page)).not_to_be_visible()
+    expect(ccf_controls_div_locator(page)).not_to_be_visible()
+    ccf_compute_button_locator(page).click()
+    expect(ccf_density_threshold_label_locator(page)).to_be_visible(timeout=10000)
 #    expect(ccf_controls_div_locator(page)).to_be_visible()
 #
 #    expect(ccf_1st_line_start_label_locator(page)).not_to_be_visible()
@@ -93,12 +104,16 @@ def test_app_title(page : Page):
 #    pv_parameter_selection_radio_on_locator(page).click()
 #    expect(pv_parameter_selection_locator(page)).to_be_visible()
 #
-#
 #    assert pi._chosen_parameters() is None
 #    pv_export_parameters_button_locator(page).click()
 #    time.sleep(1)
 #    params = pi._chosen_parameters()
 #
-#    assert params['n_clusters'] == default_params['n_clusters']
-#    np.testing.assert_almost_equal(np.array(params['start']), np.array(default_params['start']))
-#    np.testing.assert_almost_equal(np.array(params['end']), np.array(default_params['end']))
+#    print(params)
+#    assert params["n_clusters"] == default_params["n_clusters"]
+#    np.testing.assert_almost_equal(
+#        np.array(params["start"]), np.array(default_params["start"])
+#    )
+#    np.testing.assert_almost_equal(
+#        np.array(params["end"]), np.array(default_params["end"])
+#    )
