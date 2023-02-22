a27a93c114ee77cb4997fc75ddbacf7ac1d71433
Luis Scoccola <luis.scoccola@gmail.com>
2023-02-21 22:13:13 -0500

    try with older ubuntu

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ def readme():
 
 setup(
     name="persistable-clustering",
-    version="0.3.2.4",
+    version="0.3.2.5",
     description="Density-based clustering for exploratory data analysis based on multi-parameter persistence",
     long_description=readme(),
     long_description_content_type="text/markdown",
