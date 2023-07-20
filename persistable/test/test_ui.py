573febb4b02c492f8ce4ca04071d8f187eff440b
Luis Scoccola <luis.scoccola@gmail.com>
2023-07-20 12:56:22 -0400

    test: add basic save ui test

--- persistable/test/test_ui.py
+++ persistable/test/test_ui.py
@@ -106,6 +106,75 @@ def test_app_title(page: Page):
 
     expect(page).to_have_title(re.compile("Persistable"))
 
+def test_save_ui(page: Page):
+
+    # TODO: set timeout globally
+    timeout_milliseconds = 60000
+
+    page.goto(url)
+
+    # ccf
+    ccf_details_locator(page).click()
+    ccf_granularity_input_locator(page).fill("4")
+
+    expect(ccf_density_threshold_label_locator(page)).not_to_be_visible(timeout=timeout_milliseconds)
+    expect(ccf_controls_div_locator(page)).not_to_be_visible(timeout=timeout_milliseconds)
+    ccf_compute_button_locator(page).click()
+    expect(ccf_density_threshold_label_locator(page)).to_be_visible(timeout=timeout_milliseconds)
+    expect(ccf_controls_div_locator(page)).to_be_visible(timeout=timeout_milliseconds)
+
+    expect(ccf_1st_line_start_label_locator(page)).not_to_be_visible(timeout=timeout_milliseconds)
+    ccf_vineyard_input_selection_radio_on_locator(page).click(timeout=timeout_milliseconds)
+    expect(ccf_1st_line_start_label_locator(page)).to_be_visible(timeout=timeout_milliseconds)
+
+    # pv
+    pv_details_locator(page).click()
+    pv_granularity_input_locator(page).fill("2")
+
+    expect(pv_prominence_label_locator(page)).not_to_be_visible(timeout=timeout_milliseconds)
+    pv_compute_button_locator(page).click()
+    expect(pv_prominence_label_locator(page)).to_be_visible(timeout=timeout_milliseconds)
+
+    expect(pv_parameter_selection_locator(page)).not_to_be_visible(timeout=timeout_milliseconds)
+    pv_parameter_selection_radio_on_locator(page).click()
+    expect(pv_parameter_selection_locator(page)).to_be_visible(timeout=timeout_milliseconds)
+
+    pv_export_parameters_button_locator(page).click()
+
+    # save and retreive ui state
+    ui_state = pi.save_ui_state()
+
+    X = make_blobs(100, centers=2, random_state=1)[0]
+    p2 = persistable.Persistable(X, debug=True, threading=True)
+    pi2 = persistable.PersistableInteractive(p2)
+    port2  = pi2.start_ui(ui_state=ui_state, debug=True)
+    url2 = "http://localhost:" + str(port2) + "/"
+
+    time.sleep(2)
+
+    page.goto(url2)
+
+    ccf_details_locator(page).click()
+    ccf_compute_button_locator(page).click()
+    ccf_vineyard_input_selection_radio_on_locator(page).click(timeout=timeout_milliseconds)
+
+    pv_details_locator(page).click()
+    pv_compute_button_locator(page).click()
+    pv_parameter_selection_radio_on_locator(page).click()
+    pv_export_parameters_button_locator(page).click()
+
+    time.sleep(2)
+
+    params = pi2._chosen_parameters()
+
+    assert params["n_clusters"] == default_params["n_clusters"]
+    np.testing.assert_almost_equal(
+        np.array(params["start"]), np.array(default_params["start"])
+    )
+    np.testing.assert_almost_equal(
+        np.array(params["end"]), np.array(default_params["end"])
+    )
+
 
 def test_end_to_end(page: Page):
 
